af90a645efded47e28afa210d64106ffd41c1dfe
vnijs <radiant@rady.ucsd.edu>
2017-04-08 15:28:10 -0700

    report short-cut

--- inst/app/www/js/run_return.js
+++ inst/app/www/js/run_return.js
@@ -3,5 +3,25 @@
 $(document).keydown(function(event) {
   if ($("#clt_resample").is(":visible") && (event.metaKey || event.ctrlKey) && event.keyCode == 13) {
     $("#clt_resample").click();
-  } 
+  }
+});
+
+$(document).keydown(function(event) {
+  if ($("#compare_means_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#compare_means_report").click();
+  } else if ($("#compare_props_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#compare_props_report").click();
+  } else if ($("#correlation_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#correlation_report").click();
+  } else if ($("#cross_tabs_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#cross_tabs_report").click();
+  } else if ($("#goodness_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#goodness_report").click();
+  } else if ($("#prob_calc_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#prob_calc_report").click();
+  } else if ($("#single_mean_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#single_mean_report").click();
+  } else if ($("#single_prop_report").is(":visible") && event.altKey && event.keyCode == 13) {
+    $("#single_prop_report").click();
+  }
 });
