727de565dd3a109b0b7663e2342f23345cd804c8
vnijs <radiant@rady.ucsd.edu>
2018-01-08 13:31:36 -0800

    launch

--- R/radiant.R
+++ R/radiant.R
@@ -1,32 +1,25 @@
-#' Launch radiant.basics in default browser or Rstudio Viewer
+#' Launch radiant.basics in default browser
 #'
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
-#' @param run Run radiant.basics in an external browser ("browser") or in the Rstudio viewer ("viewer")
-#'
-#' @importFrom rstudioapi viewer
+#' @importFrom radiant.data launch
 #'
 #' @examples
 #' \dontrun{
-#' radiant.basics::radiant.basics()
-#' radiant.basics::radiant.basics("viewer")
+#' radiant.basics()
 #' }
-#'
 #' @export
-radiant.basics <- function(run = "browser") {
-  if (!"package:radiant.basics" %in% search()) {
-    if (!sshhr(require(radiant.basics))) {
-      stop("\nCalling radiant.basics start function but radiant.basics is not installed.")
-    }
-  }
-  run <- if (run == "viewer") {
-    message("\nStarting radiant.basics in Rstudio Viewer ...")
-    rstudioapi::viewer
-  } else {
-    message("\nStarting radiant.basics in default browser ...\n\nUse radiant.basics::radiant.basics(\"viewer\") to open radiant.basics in Rstudio Viewer")
-    TRUE
-  }
-  suppressPackageStartupMessages(
-    shiny::runApp(system.file("app", package = "radiant.basics"), launch.browser = run)
-  )
-}
+radiant.basics <- function() radiant.data::launch(package = "radiant.basics", run = "browser")
+
+#' Launch radiant.basics in the Rstudio viewer
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
+#'
+#' @importFrom radiant.data launch
+#'
+#' @examples
+#' \dontrun{
+#' radiant.basics_viewer()
+#' }
+#' @export
+radiant.basics_viewer <- function() radiant.data::launch(package = "radiant.basics", run = "viewer")
