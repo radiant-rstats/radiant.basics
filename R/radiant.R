05aec6b287282ba87777b6c0d8b4ca844d1d27ab
vnijs <radiant@rady.ucsd.edu>
2018-03-30 22:27:47 -0700

    launch in Rstudio window

--- R/radiant.R
+++ R/radiant.R
@@ -1,4 +1,4 @@
-#' Launch radiant.basics in default browser
+#' Launch radiant.basics in the default browser
 #'
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
@@ -10,6 +10,19 @@
 #' }
 #' @export
 radiant.basics <- function() radiant.data::launch(package = "radiant.basics", run = "browser")
+
+#' Launch radiant.basics in an Rstudio window
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
+#'
+#' @importFrom radiant.data launch
+#'
+#' @examples
+#' \dontrun{
+#' radiant.basics_window()
+#' }
+#' @export
+radiant.basics_window <- function() radiant.data::launch(package = "radiant.basics", run = "window")
 
 #' Launch radiant.basics in the Rstudio viewer
 #'
