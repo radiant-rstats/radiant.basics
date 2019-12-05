9f31ec2354354bed347dde28d863ce92fd8d4a88
vnijs <radiant@rady.ucsd.edu>
2019-12-05 00:33:47 -0800

    patchwork

--- R/aaa.R
+++ R/aaa.R
@@ -19,7 +19,6 @@ globalVariables(c(
 #' @importFrom magrittr %>% %<>% set_rownames set_colnames set_names divide_by
 #' @importFrom graphics pairs par points strwidth text
 #' @importFrom patchwork wrap_plots plot_annotation
-#' @importFrom gridExtra grid.arrange
 #' @importFrom stats na.omit binom.test chisq.test cor.test cov dbinom dchisq dexp df dnorm dpois dt dunif p.adjust pbinom pchisq pexp pf pnorm ppois prop.test pt punif qbinom qchisq qexp qf qpois qt qunif qnorm rbinom dlnorm plnorm qlnorm relevel sd setNames symnum t.test wilcox.test
 #' @importFrom utils combn
 #' @importFrom import from
